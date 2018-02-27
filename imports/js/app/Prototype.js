b91d1d5b1fd883f826f380182706ce7fc34888e3
David Para <david.para@avtechcorp.com>
2018-02-27 00:40:39 -0600

    setup the login screen and demo functionality, fixed import issues, passed through Proto.js

--- imports/js/app/Prototype.js
+++ imports/js/app/Prototype.js
@@ -1,17 +1,41 @@
-import { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
 import Profile from '../profiles/ProfilePageView';
 import {loadedProfile} from "../profiles/ProfilePageView";
+import LoginComponent from '../verification/LoginView';
+import LogoComponent from './Logo';
 
-export default class Prototype extends Component {
-    static getProfile() {
-        return (alert('Profile'));
-    }
+var signedIn = false;
 
-    renderProfile() {
-        return this.getProfile();
-    }
+export default class Prototype extends React.Component {
 
-    render() {
-        this.renderProfile();
+    // Loads the initial page (Login or Profile)
+    static startup() {
+        if (signedIn === true) {
+            renderHome()
+        } else {
+            renderLogin()
+        }
     }
+}
+
+function renderLogin() {
+    // Renders the login page
+    render(<LoginComponent/>, document.getElementById('login-component'));
+    render(<LogoComponent/>, document.getElementById('logo-component'));
+}
+
+function renderHome() {
+    // TODO: render home page
+    window.open("https://www.depaul.edu/");
+    window.close();
+}
+
+function renderProfile() {
+    // TODO: render profile page
+}
+
+export function setSignedIn(bool) {
+    signedIn = bool;
+    renderHome();
 }
--- imports/js/verification/LoginView.js
+++ imports/js/verification/LoginView.js
@@ -1,12 +1,32 @@
-export let login;
-export let userId;
+import React from 'react';
 
+import style from '../../css/verification/LoginSheet.css';
+import { setSignedIn } from '../app/Prototype';
 
-login = function check(form) {
-    userId = form.userId.value;
-    if (userId === 'dpara' && form.pswrd.value === "123") {
-        window.open('http://www.depaul.edu/');
-    } else {
-        alert("The username and password you entered did not match what is on file");
+var userId;
+var password;
+
+export default class LoginComponent extends React.Component {
+    render () {
+        return (
+            <form>
+                <div className={style.login}>
+                    <input id="userId" type="text" placeholder="username" /><br/>
+                    <input id="password" type="password" placeholder="password" /><br/>
+                    <input type="submit" value="Login" onClick={this.login} /><br/>
+                    <a href="http://www.depaul.edu">Sign Up?</a>
+                </div>
+            </form>
+        );
     }
-};
+
+    login() {
+        userId = document.getElementById("userId").value;
+        password = document.getElementById("password").value;
+        if (userId === "admin" && password === "123") {
+            setSignedIn(true);
+        } else {
+            alert("The username and password you entered did not match what is on file");
+        }
+    }
+}
