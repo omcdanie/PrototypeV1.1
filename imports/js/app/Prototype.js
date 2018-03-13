8c7778aa3d756d334545fe79bf919fc3739abd22
Ola McDaniel <omcdaniel1@gmail.com>
2018-03-12 19:31:52 -0500

    A bunch of changes to Buckets and Profile page.  Extensive database work still needs to happen.

--- imports/js/app/Prototype.js
+++ imports/js/app/Prototype.js
@@ -4,6 +4,7 @@ import {Route, HashRouter} from 'react-router-dom';
 import LoginComponent from '../verification/LoginView';
 import BannerComponent from './Banner';
 import Home from '../homePage/HomePageView';
+import Profile from '../profiles/ProfilePageView';
 
 let isSignedIn = false;
 
@@ -16,6 +17,7 @@ export default class Prototype extends Component {
                     <Route path='/' component={BannerComponent} />
                     <Route path='/login' component={LoginComponent} />
                     <Route path='/home' component={Home} />
+                    <Route path='/Profile' component={Profile} />
                 </div>
             </HashRouter>
         );
